b5bacd0410a2768ef6d3e2ed48b96f7e82c1c6e3
ka <ka.kaosf@gmail.com>
2018-05-16 17:51:53 +0900

    Tweak test code

--- sanitize_spec.rb
+++ sanitize_spec.rb
@@ -2,17 +2,15 @@ require 'rspec'
 require_relative 'core'
 
 RSpec.describe "DecoMailFilter" do
-  before do
-    @stdout_string = DecoMailFilter.work mail
-  end
-
   test_address = 'test@example.com'
   test_address_2 = 'test2@example.com'
 
-  describe "\"x-mail-filter\" header" do
-    subject { MailParser::Message.new(mail_for_test).header.raw('x-mail-filter')&.map(&:chomp) }
+  let(:mail_after) { DecoMailFilter.work mail_before }
 
-    let(:mail) do
+  describe "\"x-mail-filter\" header" do
+    subject { MailParser::Message.new(mail).header.raw('x-mail-filter')&.map(&:chomp) }
+
+    let(:mail_before) do
       <<~EOF
       To: #{test_address}
       Subject: Test subject
@@ -23,21 +21,21 @@ RSpec.describe "DecoMailFilter" do
     end
 
     describe "Original mail does not have it" do
-      let(:mail_for_test) { mail }
+      let(:mail) { mail_before }
       it { is_expected.to be_nil }
     end
 
     describe "Filtered mail has it" do
-      let(:mail_for_test) { @stdout_string }
+      let(:mail) { mail_after }
       it { is_expected.to eq ["DECO Mail Filter"] }
     end
   end
 
   describe "To address" do
-    subject { MailParser::Message.new(@stdout_string).header.raw('to').map(&:chomp) }
+    subject { MailParser::Message.new(mail_after).header.raw('to').map(&:chomp) }
 
     describe "1 To address doesn't change" do
-      let(:mail) do
+      let(:mail_before) do
         <<~EOF
         To: #{test_address}
         Subject: Test subject
@@ -51,7 +49,7 @@ RSpec.describe "DecoMailFilter" do
     end
 
     describe "2 To addresses changes to 1 dummy address" do
-      let(:mail) do
+      let(:mail_before) do
         <<~EOF
         To: #{test_address}, #{test_address_2}
         Subject: Test subject
@@ -65,7 +63,7 @@ RSpec.describe "DecoMailFilter" do
     end
 
     describe "1 To address and 1 CC address change to 1 dummy address" do
-      let(:mail) do
+      let(:mail_before) do
         <<~EOF
         To: #{test_address}
         Cc: #{test_address_2}
@@ -81,7 +79,7 @@ RSpec.describe "DecoMailFilter" do
   end
 
   describe "Cc address" do
-    let(:mail) do
+    let(:mail_before) do
       <<~EOF
       To: #{test_address}
       Cc: #{test_address_2}
@@ -92,6 +90,6 @@ RSpec.describe "DecoMailFilter" do
       EOF
     end
 
-    it { expect(MailParser::Message.new(@stdout_string).header['cc']).to be_nil }
+    it { expect(MailParser::Message.new(mail_after).header['cc']).to be_nil }
   end
 end
