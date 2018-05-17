2142a11b873762921e3393e5b26b290bb95f74ec
ka <ka.kaosf@gmail.com>
2018-05-17 11:52:18 +0900

    Fix Syslog hard coding
    Replace them with @logger variable and its default value is NullLogger instance.

--- core.rb
+++ core.rb
@@ -1,11 +1,5 @@
 require 'mailparser'
-require 'syslog'
-include Syslog::Constants
-require 'logger'
-
-Syslog.open("DECO_MF", LOG_PID|LOG_NDELAY, LOG_MAIL) unless Syslog.opened?
-Syslog.info("start")
-Syslog.info("RCPT TO: [#{ARGV[0].chop}]")
+require 'null_logger'
 
 class DecoMailFilter
   DUMMY_MAIL_TO = 'bcc@deco-project.org'
@@ -14,20 +8,26 @@ class DecoMailFilter
     @bcc_conversion = bcc_conversion
   end
 
+  attr_accessor :logger
+
+  def logger
+    @logger ||= NullLogger.new
+  end
+
   def work input
     mail = MailParser::Message.new(input)
 
-    Syslog.info("From: #{mail.from}")
+    logger.info("From: #{mail.from}")
 
     # To: の数確認
     flag_to = false
     flag_to = true if mail.to.size > 1
-    Syslog.info("To size: #{mail.to.size}")
+    logger.info("To size: #{mail.to.size}")
 
     # Cc: の存在確認
     flag_cc = false
     flag_cc = true if mail.cc.size >= 1
-    Syslog.info("Cc size: #{mail.cc.size}")
+    logger.info("Cc size: #{mail.cc.size}")
 
     # BCC変換が無効の場合
     # 宛先ドメインが全て同じであることの確認
@@ -44,10 +44,10 @@ class DecoMailFilter
     mail.header.keys.each do | key |
       if key == 'to' && (flag_to || flag_cc)
         output += "#{key}: #{DUMMY_MAIL_TO}\r\n"
-        Syslog.info("fix To: #{DUMMY_MAIL_TO}")
+        logger.info("fix To: #{DUMMY_MAIL_TO}")
       elsif key == 'cc'
         # drop
-        Syslog.info("remove Cc")
+        logger.info("remove Cc")
       else
         if mail.header.raw(key).kind_of?(Array)
           mail.header.raw(key).each do | item |
@@ -60,7 +60,7 @@ class DecoMailFilter
     end
     # body
     output += mail.rawbody
-    Syslog.info("end")
+    logger.info("end")
     output
   end
 end
--- sanitize.rb
+++ sanitize.rb
@@ -3,6 +3,16 @@
 #
 # Copyright (C) 2017 New Media Tokushima Co.,Ltd. All rights reserved.
 
+require 'syslog'
+include Syslog::Constants
+require 'logger'
+
+Syslog.open("DECO_MF", LOG_PID|LOG_NDELAY, LOG_MAIL) unless Syslog.opened?
+Syslog.info("start")
+Syslog.info("RCPT TO: [#{ARGV[0].chop}]")
+
 require_relative 'core'
 
-print DecoMailFilter.new.work $stdin.read
+filter = DecoMailFilter.new
+filter.logger = Syslog
+print filter.work $stdin.read
