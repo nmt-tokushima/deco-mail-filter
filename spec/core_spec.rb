7ffaf196dc5c3d76496499c8672965bc6c20d14b
ka <ka.kaosf@gmail.com>
2018-05-22 17:54:30 +0900

    Fix describe structure in core_spec.rb

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -1,43 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe "DecoMailFilter::Core" do
-  test_address = '<test@example.com>'
-  test_address_2 = '<test2@example.com>'
+  describe "#work" do
+    test_address = '<test@example.com>'
+    test_address_2 = '<test2@example.com>'
 
-  let(:config) { DecoMailFilter::Config.new }
-  # let(:filter) { DecoMailFilter::Core.new config = config } # ??? config becomes nil
-  let(:filter) { DecoMailFilter::Core.new config }
+    let(:config) { DecoMailFilter::Config.new }
+    # let(:filter) { DecoMailFilter::Core.new config = config } # ??? config becomes nil
+    let(:filter) { DecoMailFilter::Core.new config }
+    let(:mail_after) { filter.work mail_before }
 
-  let(:mail_after) { filter.work mail_before }
+    describe "\"x-mail-filter\" header" do
+      subject { MailParser::Message.new(mail).header.raw('x-mail-filter')&.map(&:chomp) }
 
-  describe "\"x-mail-filter\" header" do
-    subject { MailParser::Message.new(mail).header.raw('x-mail-filter')&.map(&:chomp) }
-
-    let(:mail_before) do
-      <<~EOF
-      To: #{test_address}
-      Subject: Test subject
-      Content-type: text/plain
-      
-      Test body
-      EOF
-    end
-
-    describe "Original mail does not have it" do
-      let(:mail) { mail_before }
-      it { is_expected.to be_nil }
-    end
-
-    describe "Filtered mail has it" do
-      let(:mail) { mail_after }
-      it { is_expected.to eq ["DECO Mail Filter"] }
-    end
-  end
-
-  describe "To address" do
-    subject { MailParser::Message.new(mail_after).header.raw('to').map(&:chomp) }
-
-    describe "1 To address doesn't change" do
       let(:mail_before) do
         <<~EOF
         To: #{test_address}
@@ -48,91 +23,65 @@ RSpec.describe "DecoMailFilter::Core" do
         EOF
       end
 
-      it { is_expected.to eq [test_address] }
-    end
-
-    describe "2 To addresses changes to 1 dummy address" do
-      let(:mail_before) do
-        <<~EOF
-        To: #{test_address}, #{test_address_2}
-        Subject: Test subject
-        Content-type: text/plain
-        
-        Test body
-        EOF
+      describe "Original mail does not have it" do
+        let(:mail) { mail_before }
+        it { is_expected.to be_nil }
       end
 
-      it { is_expected.to eq [DecoMailFilter::DUMMY_MAIL_TO] }
+      describe "Filtered mail has it" do
+        let(:mail) { mail_after }
+        it { is_expected.to eq ["DECO Mail Filter"] }
+      end
     end
 
-    describe "1 To address and 1 CC address change to 1 dummy address" do
-      let(:mail_before) do
-        <<~EOF
-        To: #{test_address}
-        Cc: #{test_address_2}
-        Subject: Test subject
-        Content-type: text/plain
-        
-        Test body
-        EOF
+    describe "To address" do
+      subject { MailParser::Message.new(mail_after).header.raw('to').map(&:chomp) }
+
+      describe "1 To address doesn't change" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
+
+        it { is_expected.to eq [test_address] }
       end
 
-      it { is_expected.to eq [DecoMailFilter::DUMMY_MAIL_TO] }
-    end
-  end
+      describe "2 To addresses changes to 1 dummy address" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}, #{test_address_2}
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
 
-  describe "Cc address" do
-    let(:mail_before) do
-      <<~EOF
-      To: #{test_address}
-      Cc: #{test_address_2}
-      Subject: Test subject
-      Content-type: text/plain
-      
-      Test body
-      EOF
-    end
-
-    it { expect(MailParser::Message.new(mail_after).header['cc']).to be_nil }
-  end
-
-  describe "To and Cc Addresses when BCC conversion is disabled" do
-    let(:config) { DecoMailFilter::Config.new bcc_conversion = false }
-
-    subject { MailParser::Message.new(mail_after).to.map(&:to_s) }
-
-    context "2 To addresses" do
-      let(:mail_before) do
-        <<~EOF
-        To: #{test_address}, #{test_address_2}
-        Subject: Test subject
-        Content-type: text/plain
-        
-        Test body
-        EOF
+        it { is_expected.to eq [DecoMailFilter::DUMMY_MAIL_TO] }
       end
 
-      it { is_expected.to include test_address }
-      it { is_expected.to include test_address_2 }
-    end
+      describe "1 To address and 1 CC address change to 1 dummy address" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}
+          Cc: #{test_address_2}
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
 
-    context "Different domains" do
-      let(:mail_before) do
-        <<~EOF
-        To: #{test_address}, <test@another.com>
-        Subject: Test subject
-        Content-type: text/plain
-        
-        Test body
-        EOF
+        it { is_expected.to eq [DecoMailFilter::DUMMY_MAIL_TO] }
       end
-
-      it { is_expected.to include "<#{DecoMailFilter::DUMMY_MAIL_TO}>" }
-      it { is_expected.not_to include test_address }
-      it { is_expected.not_to include test_address_2 }
     end
 
-    context "1 To address and 1 Cc address" do
+    describe "Cc address" do
       let(:mail_before) do
         <<~EOF
         To: #{test_address}
@@ -144,11 +93,60 @@ RSpec.describe "DecoMailFilter::Core" do
         EOF
       end
 
-      it { is_expected.to include test_address }
-      it { expect(MailParser::Message.new(mail_after).cc).to eq [] }
+      it { expect(MailParser::Message.new(mail_after).header['cc']).to be_nil }
     end
-  end
 
-  describe 'Encrypt attachments' do
+    describe "To and Cc Addresses when BCC conversion is disabled" do
+      let(:config) { DecoMailFilter::Config.new bcc_conversion = false }
+
+      subject { MailParser::Message.new(mail_after).to.map(&:to_s) }
+
+      context "2 To addresses" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}, #{test_address_2}
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
+
+        it { is_expected.to include test_address }
+        it { is_expected.to include test_address_2 }
+      end
+
+      context "Different domains" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}, <test@another.com>
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
+
+        it { is_expected.to include "<#{DecoMailFilter::DUMMY_MAIL_TO}>" }
+        it { is_expected.not_to include test_address }
+        it { is_expected.not_to include test_address_2 }
+      end
+
+      context "1 To address and 1 Cc address" do
+        let(:mail_before) do
+          <<~EOF
+          To: #{test_address}
+          Cc: #{test_address_2}
+          Subject: Test subject
+          Content-type: text/plain
+          
+          Test body
+          EOF
+        end
+
+        it { is_expected.to include test_address }
+        it { expect(MailParser::Message.new(mail_after).cc).to eq [] }
+      end
+    end
   end
 end
