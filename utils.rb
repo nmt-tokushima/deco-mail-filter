b8ef3366ef6f476a3d98e586dc7e2eec93ab5276
ka <ka.kaosf@gmail.com>
2018-05-28 17:52:29 +0900

    Fix DecoMailFilter::Utils.zipfile_encrypted?
    It's a makeshift.

--- utils.rb
+++ utils.rb
@@ -1,22 +1,17 @@
-require 'tmpdir'
 require 'zip'
 require 'fileutils'
 
 class DecoMailFilter::Utils
   def self.zipfile_encrypted? zippath
-    tmpdir = Dir.mktmpdir
     Zip::InputStream.open zippath, 0 do |input|
       while entry = input.get_next_entry
-        save_path = File.join tmpdir, entry.name
-        File.open save_path, 'wb' do |f|
-          f.puts input.read # Raise Zlib::DataError if this is encrypted
-        end
+        # If at least 1 file which is not encrypted exists,
+        # the result of this method should be false. (probabbly...)
+        return false if entry.gp_flags == 0
+        # NOTE: `entry.gp_flags == 9` means encrypted.
+        # TODO: Research that `entry.gp_flags == 9` is the correct condition.
       end
     end
-    false
-  rescue Zlib::DataError => e
     true
-  ensure
-    FileUtils.rm_rf tmpdir
   end
 end
