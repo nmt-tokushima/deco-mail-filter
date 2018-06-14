f0876a0ad4378ddf0b9b68ff86e58cbb3be5a33d
ka <ka.kaosf@gmail.com>
2018-06-14 12:55:08 +0900

    Fix BCC conversion

--- src/core.rb
+++ src/core.rb
@@ -51,25 +51,28 @@ module DecoMailFilter
 
       logger.info("From: #{mail.from}")
 
-      # To: の数確認
       flag_to = false
-      flag_to = true if mail.to.size > 1
-      logger.info("To size: #{mail.to.size}")
-
-      # Cc: の存在確認
       flag_cc = false
-      flag_cc = true if mail.cc.size >= 1
-      logger.info("Cc size: #{mail.cc.size}")
+      logger.info("BCC conversion: #{@bcc_conversion}")
+      if @bcc_conversion
+        # To: の数確認
+        flag_to = true if mail.to.size > 1
+        logger.info("To size: #{mail.to.size}")
 
-      # BCC変換が無効の場合
-      if !@bcc_conversion
-        domains = (mail.to.map(&:domain) + mail.cc.map(&:domain))
-        # 宛先ドメインが全て同じであることの確認
-        if domains.uniq.size == 1
-          # 指定されたドメインであることの確認
-          if @bcc_conversion_whitelist.include? domains.first
-            flag_to = false
-            flag_cc = false
+        # Cc: の存在確認
+        flag_cc = true if mail.cc.size >= 1
+        logger.info("Cc size: #{mail.cc.size}")
+
+        # BCC変換を無効にする特例判定
+        if flag_to || flag_cc
+          domains = (mail.to.map(&:domain) + mail.cc.map(&:domain))
+          # 宛先ドメインが全て同じであることの確認
+          if domains.uniq.size == 1
+            # 指定されたドメインであることの確認
+            if @bcc_conversion_whitelist.include? domains.first
+              flag_to = false
+              flag_cc = false
+            end
           end
         end
       end
